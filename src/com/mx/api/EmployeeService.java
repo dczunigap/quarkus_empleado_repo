4948f744d2a27a8ff652a497f18c8157e9e33460
David Candelario Zúñiga Pérez <dczunigap@outlook.com>
2026-06-10 16:52:55 -0600

    Se agrega Swagger

--- src/com/mx/api/EmployeeService.java
+++ src/com/mx/api/EmployeeService.java
@@ -5,6 +5,9 @@ import jakarta.ws.rs.core.Response;
 
 import java.util.List;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+
 import com.mx.business.EmployeeBusiness;
 import com.mx.entity.EmployeeEntity;
 
@@ -33,6 +36,9 @@ public class EmployeeService {
 
     @GET
     @Path("/{id}")
+    @Operation(summary = "Get employee by EmployeeID", description = "Find employee using Employee ID.")
+    @APIResponse(responseCode = "200", description = "Employee found successfully")
+    @APIResponse(responseCode = "404", description = "The employee with the requested ID does not exist")
     public EmployeeEntity getById(@PathParam("id") Long id) {
         return employeeBusiness.getEmployeeById(id);
     }
@@ -45,12 +51,18 @@ public class EmployeeService {
 
     @PATCH
     @Path("/{id}")
+    @Operation(summary = "Update employee", description = "Updates an existing employee's information.")
+    @APIResponse(responseCode = "200", description = "Employee updated successfully")
+    @APIResponse(responseCode = "404", description = "The employee with the requested ID does not exist")
     public EmployeeEntity update(@PathParam("id") Long id, EmployeeEntity employee) {
         return employeeBusiness.actualizarEmpleado(id, employee);
     }
 
     @DELETE
     @Path("/{id}")
+    @Operation(summary = "Delete employee", description = "Deletes an existing employee.")
+    @APIResponse(responseCode = "204", description = "Employee deleted successfully")
+    @APIResponse(responseCode = "404", description = "The employee with the requested ID does not exist")
     public Response delete(@PathParam("id") Long id) {
         employeeBusiness.deleteEmployee(id);
         return Response.noContent().build();
